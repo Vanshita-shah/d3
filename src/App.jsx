5d17037217e8686771e4390df0ca70ff8d93e6ab
vanshitaa-shah <vanshita.s@simformsolutions.com>
2023-11-09 08:15:43 +0530

    added scatterplots

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Scatterplot from "./examples/Scatterplot";
+import Scatterplot2 from "./examples/Scatterplot2";
+
+function App() {
+  return (
+    <>
+      {/* <Selections/> */}
+      {/* <Scatterplot /> */}
+      <Scatterplot2/>
+
+    </>
+  );
+}
+
+export default App;
